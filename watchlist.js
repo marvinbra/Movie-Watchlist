6b06edaa7c9c07072bf484e71e233fce25c79ad3
marvinbra <marvinbopp37@gmail.com>
2023-10-28 11:06:30 +0200

    display movies in watchlist
    js

--- watchlist.js
+++ watchlist.js
@@ -1,10 +1,37 @@
 const mainWatchlist = document.getElementById("watchlist-main")
-let displayMovie = JSON.parse(localStorage.getItem("movieDataObj"))
+const deleteBtn = document.getElementById("remove-btn")
+let data = JSON.parse(localStorage.getItem("movieDataObj"))
 
 function render() {
-    for (let i = 0; i < displayMovie.length; i++) {
-        mainWatchlist.textContent += `${displayMovie[i].Title}` 
+    for (let i = 0; i < data.length; i++) {
+        console.log(data)
+           mainWatchlist.innerHTML += `
+                            <div class="movie-container">
+                                <div class="poster-container">
+                                    <img class="film-img" src="${data[i].Poster}" />
+                                </div>
+                                <div class="movie-details-container">
+                                    <div class="title-rating">
+                                        <h2>${data[i].Title}</h2>
+                                        <p>⭐${data[i].imdbRating}</p>
+                                    </div>
+                                    <div class="time-genre">
+                                        <p class="time">${data[i].Runtime}</p>
+                                        <p class="time">${data[i].Genre}</p>
+                                        <div class="watchlist-btn">
+                                            <button
+                                                id="main-btn" 
+                                                class="main-btn" 
+                                                data-imdb-id="${data[i].imdbID}">+
+                                            </button>
+                                            <a>Watchlist</a>
+                                        </div>
+                                    </div>
+                                    <p>${data[i].Plot}</p>
+                                </div>
+                            </div>
+                            <hr/>
+                        
+           `   
     }
 }
-
-render()
